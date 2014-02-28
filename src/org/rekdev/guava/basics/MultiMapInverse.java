1202979dee384f7c67ed5ec66d55e5008dda0b42
Bob Kuhar <Robert_Kuhar@htc.com>
2014-02-28 13:16:26 -0800

    Changes

--- src/org/rekdev/guava/basics/MultiMapInverse.java
+++ src/org/rekdev/guava/basics/MultiMapInverse.java
@@ -8,8 +8,8 @@ public class MultiMapInverse {
     public static void main( String[] args ) {
         ImmutableMultimap.Builder<String, String> builder = new ImmutableMultimap.Builder<String, String>();
         builder.putAll( "type1", Arrays.asList( "FieldA", "FieldB", "FieldC") );
-        builder.putAll( "type2", Arrays.asList( "FieldB", "FieldC", "FieldD") );
         builder.putAll( "type3", Arrays.asList( "FieldC", "FieldD", "FieldE") );
+        builder.putAll( "type2", Arrays.asList( "FieldB", "FieldC", "FieldD") );
         ImmutableMultimap<String, String> typesToFields = builder.build();
         ImmutableMultimap<String, String> fieldsToTypes = typesToFields.inverse();
         
